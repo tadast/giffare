2d4449dd313147f43fa6965bbd7a6cdc5321008c
Tadas Tamosauskas <tadas@pdfcv.com>
2014-04-15 23:38:04 +0100

    Typo

--- lib/social.rb
+++ lib/social.rb
@@ -6,8 +6,8 @@ module Social
       content = "#{gif.title}\n\n#{url}"
       if ENV['SOCIAL_SHARE']
         return if gif.shared?
-        Tweet.new(content).delay(run_at: (idx * 3).minutes_from_now).share
-        FaceBook.new(url, gif.title).delay(run_at: (idx * 3).minutes_from_now).share
+        Tweet.new(content).delay(run_at: (idx * 3).minutes.from_now).share
+        FaceBook.new(url, gif.title).delay(run_at: (idx * 3).minutes.from_now).share
         gif.update_column(:shared, true)
       else
         puts "Social sharing is off, set SOCIAL_SHARE to true to enable.\n #{content}"
